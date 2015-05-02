ba42ef70021e74199792e6068f2c10a38395d44b
Kevin Ryan <ryank15@mounties.mansfield.edu>
2015-05-01 22:04:55 -0400

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+package com.example.kevin.chevysqlite;
+
+/**
+ * Created by Kevin on 4/29/2015.
+ */
+public class Vehicle {
+    public static final String TABLE = "Vehicle";
+    public static final String ID = "id";
+    public static final String MODEL = "model";
+    public static final String YEAR = "year";
+    public static final String PRICE = "price";
+    public static final String TYPE = "type";
+
+    public int vehicle_Id;
+    public String model;
+    public int year;
+    public int price;
+    public String type;
+
+}
